f6e21713fee9727c964720d79ff90a34f32dfcc6
AnniqueG <anniquemgl@gmail.com>
2021-06-21 13:57:21 -0400

    other file necessary to test (app/src/main/java/com/example/calculator

---
+++ DivisionTester.java
@@ -0,0 +1,12 @@
+package com.example.calculator;
+
+public class DivisionTester {
+
+    public static double division(double num1, double num2){
+        double result;
+        result = num1 / num2;
+
+        return result;
+    }
+
+}
